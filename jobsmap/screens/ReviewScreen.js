57f7ec3351cadd6d49b85524c21e0e96185b2cf1
Tao Wang <tao.wangtaoau@gmail.com>
2018-02-03 22:16:30 +1100

    Added class level attributes to customize the header of review screen

--- jobsmap/screens/ReviewScreen.js
+++ jobsmap/screens/ReviewScreen.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+     // navigate is a function to navigate
+     // you can use it to navigate by giving the
+     // key of the screen defined in the app.js
+     return {
+       headerTitle: 'Review Jobs',
+       headerRight: (
+          <Button
+            title = "Settings"
+            onPress={() => navigation.navigate('settings')}
+            backgroundColor = "rgba(0,0,0,0)"
+            color = "rgba(0, 125, 125, 1)"
+          />
+        ),
+        style: {
+          marginTop: Platform.os === 'android' ? 24:0
+        }
+      };
+    }
+
   render () {
     return (
       <View>
