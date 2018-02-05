4d41bb8b9ba7bbb113f6080e89ee41c5ee073053
Tao Wang <tao.wangtaoau@gmail.com>
2018-02-05 23:14:26 +1100

    little bug fixed and added fb app id

--- jobsmap/App.js
+++ jobsmap/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
+// 576904529322770
 
 import store from './store';
 import AuthScreen from './screens/AuthScreen';
@@ -32,7 +33,7 @@ export default class App extends React.Component {
         })
       }
     });
-  
+
     return (
       <Provider store={store}>
         <MainNavigator />
--- jobsmap/store/index.js
+++ jobsmap/store/index.js
@@ -1,13 +1,13 @@
-import { createStore, compose, applyMiddleware} from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
-const store = createStore {
+const store = createStore (
   reducers,
   {},
   compose (
     applyMiddleware(thunk)
   )
-};
+);
 
 export default store;
