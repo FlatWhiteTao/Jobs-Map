55500c185b897ca905a6e5f23e5d9580feee263f
Tao Wang <tao.wangtaoau@gmail.com>
2018-02-05 23:04:36 +1100

    Added app data store and a reducer for facebook auth token state
    Any child enclosed in the provider tag will have access to the redux data store.

--- jobsmap/App.js
+++ jobsmap/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -29,9 +32,11 @@ export default class App extends React.Component {
         })
       }
     });
-
+  
     return (
-      <MainNavigator />
+      <Provider store={store}>
+        <MainNavigator />
+      </Provider>
     );
   }
 }
