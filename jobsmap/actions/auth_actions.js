7c6f2156b170c561fc4fda9c761994dcf96b35fb
Tao Wang <tao.wangtaoau@gmail.com>
2018-02-08 18:30:07 +1100

    support facebook login to the app

--- jobsmap/actions/auth_actions.js
+++ jobsmap/actions/auth_actions.js
@@ -1,7 +1,9 @@
 import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
 
 import {
-  FACEBOOK_LOGIN_SUCCESS
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
 } from './types'
 
 // AsyncStorage.setItem ('fb_token',token);
@@ -12,7 +14,23 @@ export const facebookLogin = () => async dispatch => {
   let token = await AsyncStorage.getItem ('fb_token');
   if (token) {
     // Dispatch an action saying FB is login done
+    // use payload to cache the token to the redux store
+    dispatch ({ type: FACEBOOK_LOGIN_SUCCESS, payload: token});
   }else{
     //start up Fb login process here
+    doFacebookLogin(dispatch);
   }
-}
+};
+
+const doFacebookLogin = async (dispatch) => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync('576904529322770',{
+    permissions: ['public_profile']
+  });
+
+  // something went wrong
+  if (type === 'cancel') {
+    return dispatch ({ type: FACEBOOK_LOGIN_FAIL });
+  }
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch ({ type: FACEBOOK_LOGIN_SUCCESS, payload: token});
+};
