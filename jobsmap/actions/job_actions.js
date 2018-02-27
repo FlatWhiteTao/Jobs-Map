093f452ba0727fced0a31b5cb1b53798ebfe3e09
Tao Wang <tao.wangtaoau@gmail.com>
2018-02-27 14:24:24 +1100

    added FETCH_JOB action creator to fetch jobs from indeed api
    Using the qs module to convert an object to a request url.

--- jobsmap/actions/job_actions.js
+++ jobsmap/actions/job_actions.js
@@ -1,13 +1,36 @@
 import axios from 'axios';
 import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
 
 import {
   FETCH_JOBS
 } from './types';
 
+// create an object with query params
+// and convert it to the request url by qs module
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript'
+};
+
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
+
+const buildJobsUrl = (zip) => {
+  const query = qs.stringfy({ ...JOB_QUERY_PARAMS, l: zip})
+  return `${JOB_ROOT_URL}${query}`;
+};
+
 export const fetchJobs = (region) => async (dispatch) => {
   try {
     let zip = await reverseGeocode(region);
+    const url = buildJobsUrl(zip);
+    let { data } = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data});
+    console.log(data);
   } catch (e) {
     console.error(e);
   } finally {
